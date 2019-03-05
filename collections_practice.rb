cd7d2de430636e1a252cc32d4ae1b954255a0a4b
Abe D <iaryiou111@gmail.com>
2019-03-05 01:18:27 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -88,7 +88,7 @@ def organize_schools(schools)
       sorted_schools << {city => [school]}
       puts "#{sorted_schools} updated"
     else
-      sorted_schools[city].push(school)
+      sorted_schools[city] = school
       puts "#{sorted_schools[city]} updated with #{school}"
     end
   end
